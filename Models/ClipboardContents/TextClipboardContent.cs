36d3d364b7141072087cb7ccdc731a574095f13c
HydroH <iodizon@163.com>
2023-04-20 23:25:54 +0800

    Use MVVM pattern.

---
+++ Models/ClipboardContents/TextClipboardContent.cs
@@ -0,0 +1,15 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClipExtended.Models.ClipboardContents
+{
+    public partial class TextClipboardContent: ClipboardContent
+    {
+        [ObservableProperty]
+        private string text;
+    }
+}
---
+++ ViewModels/ClipboardListViewModel.cs
@@ -0,0 +1,26 @@
+﻿using ClipExtended.Models.ClipboardContents;
+using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClipExtended.ViewModels
+{
+    public partial class ClipboardListViewModel: ObservableObject
+    {
+        public readonly ObservableCollection<ClipboardContent> Items = new();
+
+        public void Add(ClipboardContent item)
+        {
+            Items.Add(item);
+        }
+
+        public void RemoveAt(int index)
+        {
+            Items.RemoveAt(index);
+        }
+    }
+}
